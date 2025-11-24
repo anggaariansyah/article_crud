e263072d999510333fb0bd31353c2c0c86a03309
anggaariansyah <33474788+anggaariansyah@users.noreply.github.com>
2025-11-25 03:45:23 +0700

    Update article_photo.go

--- models/article_photo.go
+++ models/article_photo.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type ArticlePhoto struct {
-	ID        uint      `gorm:"primaryKey"`
-	ArticleID uint      `gorm:"not null"`
-	URL       string    `gorm:"not null"`
-	Article   Article   `gorm:"constraint:OnDelete:CASCADE;"`
+	ID        uint   `gorm:"primaryKey" json:"id"`
+	ArticleID uint   `gorm:"not null" json:"article_id"`
+	URL       string `gorm:"not null" json:"url"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
