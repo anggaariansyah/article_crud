f68a81d7c48740fb75b03f912edfd1d7819f21ef
anggaariansyah <33474788+anggaariansyah@users.noreply.github.com>
2025-11-25 03:46:08 +0700

    Update migrate.go

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -6,12 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type ArticlePhoto struct {
-	ID        uint   `gorm:"primaryKey"`
-	ArticleID uint   `gorm:"not null;index"`
-	URL       string `gorm:"not null"`
-}
-
 func Migrate(db *gorm.DB) {
 	db.AutoMigrate(&models.Article{}, &models.ArticlePhoto{})
 }
